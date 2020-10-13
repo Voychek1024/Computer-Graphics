ddf324aeaa7e5a7b559c93b6906081b151bcd540
Quartz <oncwnuNb4afaT285dwq0UHFKnZu0@git.weixin.qq.com>
2020-10-13 20:22:13 +0800

    ui design commit

--- ex02/start02.py
+++ ex02/start02.py
@@ -1,5 +1,26 @@
-import time
+import sys
+
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+from ex02.ex02_ui import *
+
+
+class MainWindow(QMainWindow, Ui_Window_2):
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent)
+        self.setupUi(self)
+
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle("Computer Graphics EX01")
+        self.setWindowIcon(QIcon("02.png"))
+        self.show()
+
 
 if __name__ == '__main__':
-    time.sleep(5)
-    print("1.")
+    app_2 = QApplication(sys.argv)
+    mywin = MainWindow()
+    mywin.show()
+    sys.exit(app_2.exec_())
